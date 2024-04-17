63c88b6f796d76f6f266e730afeb78b72158c2ca
PumPuiEvo <piyungkoon.s@kkumail.com>
2024-04-17 10:10:44 +0700

    gradle AR

--- ardemo/test/widget_test.dart
+++ ardemo/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ardemo/main.dart';
+import 'package:ardemo/mainTest.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

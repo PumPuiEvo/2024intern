7c6b9a4722a181ea63ba8d2c98c034b3d87c99b3
PumPuiEvo <piyungkoon.s@kkumail.com>
2024-04-18 19:01:42 +0700

    temporaly for ar-app

---
+++ ar-app2/lib/model/ar_objects.dart
@@ -0,0 +1,9 @@
+class ARObjects {
+  static const String fox = "assets/anubis_head/anubis_head.gltf";
+  static const String figure = "assets/porsche911/scene.gltf";
+  static const String shoe = "assets/Chicken_01/Chicken_01.gltf";
+  static const String chair =
+      "https://github.com/KhronosGroup/glTF-Sample-Models/blob/main/2.0/SheenChair/glTF-Binary/SheenChair.glb";
+
+  static const String chicken = "assets/Chicken_01/Chicken_01.gltf";
+}
